482aa97d36bd7bedf0ec07362051954e42c319d6
DACUS1995 <studormarian@gmail.com>
2020-03-22 15:10:41 +0200

    added worker test

---
+++ worker_test.go
@@ -0,0 +1,38 @@
+package asyncexecutor
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestSimpleWorker(t *testing.T) {
+	expected := "Done"
+	newJob := NewJob(
+		/*id*/ 0,
+		func(str string) (string, error) {
+			return str, nil
+		},
+		/*paramObject*/ &parameterObject{[]interface{}{expected}},
+	)
+
+	worker := NewWorker(
+		0,
+		make(chan *Job),
+		make(chan *ResponseObject),
+	)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go worker.start(wg)
+
+	worker.jobQueue <- newJob
+	response := newJob.Await()
+
+	if response.responses[0] != expected {
+		t.Errorf("Expected: [%v] | Returned: [%v]", expected, response.responses[0])
+	}
+
+	worker.stop()
+
+	wg.Wait()
+}
